cc003369225c492dc063ef4ccf7b9858cc6a452c
Jashrutha30222 <2200030222@kluniversity.in>
2024-11-13 14:21:14 +0530

    s13

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import Appbar from './components/Appbar.js';
+import SignUp from './components/SignUp.js';
+import SignIn from './components/SignIn.js';
+import User from './components/User.js';
+import Show from './components/Show.js';
 
-function App() {
+
+function App({store}) {
+  function Page(){
+    switch(store.getState()){
+      case "Signup":
+        return (<div><Appbar store={store}/><SignUp /></div>)
+      case "Signin":
+        return (<div><Appbar store={store}/><SignIn store={store} /></div>)   
+      case "User":
+        if(localStorage.getItem("role")==1 || localStorage.getItem("role")==2)
+        return (<div><Appbar store={store}/><User/></div>)  
+        else 
+        return (<div><Appbar store={store}/><SignIn store={store} /></div>)   
+      case "Show":
+        if(localStorage.getItem("role")==1 )
+          return (<div><Appbar store={store}/><Show /></div>)
+        else
+        return (<div><Appbar store={store}/><SignIn store={store} /></div>)   
+
+
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Feedback Management System</p>        
       </header>
+      <div className='App-body'>
+        <Page />
+      </div>
     </div>
   );
 }
